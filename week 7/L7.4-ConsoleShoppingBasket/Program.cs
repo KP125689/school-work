5b18bdd053b305d620da3c890a05aca138e02b61
KP125689 <91595149+KP125689@users.noreply.github.com>
2022-01-27 12:28:05 +0000

    Add files via upload

---
+++ week 7/L7.4-ConsoleShoppingBasket/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace L7._4_ConsoleShoppingBasket
+{
+    class Program
+    { 
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.WriteLine("====================================");
+            Console.WriteLine("Happy Days Games.");
+            Console.WriteLine("====================================");
+
+            //Enter your codes here
+            string filename = "categories.csv";
+            string filename2 = "productCatalogue.csv";
+            string line;
+            string line2;
+            
+
+            string[] firstCategory = File.ReadAllLines(filename);
+            for (int count= 0; count < firstCategory.Length; count++ )
+            {
+                Console.WriteLine(count + ". " + firstCategory[count]);
+            }
+
+            Console.WriteLine("choose an option between [0-3] inclusive");
+            string choice1 = Console.ReadLine();
+
+            string[] secondCategory = File.ReadAllLines(filename2);
+            List<string> productName = new List<string>();
+            for ( int count2 = 0; count2 < secondCategory.Length; count2++)
+            {
+                string[] splits = secondCategory[count2].Split(",");
+                
+                if(splits[1] == firstCategory[int.Parse(choice1)])
+                {
+                    Console.WriteLine(secondCategory[count2]);
+                    productName.Add(secondCategory[count2]);
+                }
+                    
+            }
+
+            List<string> basket = new List<string>();
+
+            int productMax = productName.Count - 1;
+            int count3;
+
+            do
+            {
+                Console.WriteLine("choose a product from 0 to " + productMax + " inclusive");
+                count3 = int.Parse(Console.ReadLine());
+                if( count3 >= 0 && count3 <= productMax)
+                {
+                    basket.Add(productName[count3]);
+                }
+
+            }while (count3 >= 0 && count3 <= productMax);
+
+
+            Console.WriteLine("");
+            Console.WriteLine("Your basket is below");
+            Console.WriteLine("");
+
+            double[] userPrice = new double[productName.Count];
+            string[] userGame = new string[productName.Count];
+            double finalPrice = 0;
+
+            for (int count4 = 0 ; count4 < basket.Count; count4++)
+            {
+                string[] splits2 = productName[count4].Split(',');
+                Console.WriteLine(splits2[0] + " " + splits2[2]);
+                finalPrice += double.Parse(splits2[2]);
+
+            }
+            Console.WriteLine("your total is: " + finalPrice);
+            
+
+
+
+
+        }
+    }
+}
+
+
+ 
---
+++ week 7/L7.5-ConsoleHeartsMiningGame/Program.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.IO;
+
+namespace L7._5_ConsoleHeartsMiningGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            // initialise program variables
+            char spade = '\u2660';
+            char heart = '\u2665'; 
+            char playAgain = 'N';
+
+            // change text encoding from ASCII to UTF8, to enable spade and hearts symboll display
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            
+            //Enter your codes here
+            Console.WriteLine("would you like to play this game? (y/n)");
+            string letsPlay = Console.ReadLine();
+
+            do
+            {
+    
+                int heartsFound = 0;
+                int score = 0;
+                int totalHearts = 0;
+                
+                int choice = 0;  
+                bool i = true;
+                // user chooses difficulty
+                while (i == true)
+                {
+                    Console.WriteLine("select difficulty level [1-5] inclusive");
+                    int userOption = int.Parse(Console.ReadLine());
+                    switch(userOption)
+                    {
+                        case 1:
+                            Console.WriteLine("Good choice");
+                            totalHearts = 15;
+                            i = false;
+                            break;
+                        case 2:
+                            Console.WriteLine("Good choice");
+                            totalHearts = 12;
+                            i = false;
+                            break;
+                        case 3:
+                            Console.WriteLine("Good choice");
+                            totalHearts = 9;
+                            i = false;
+                            break;
+                        case 4:
+                            Console.WriteLine("Good choice");
+                            totalHearts = 6;
+                            i = false;
+                            break;
+                        case 5:
+                            Console.WriteLine("Good choice");
+                            totalHearts = 3;
+                            i = false;
+                            break;
+                        default:
+                            Console.WriteLine("please put a valid choice");
+                            break;
+
+                    }
+                    userOption = choice;
+                }
+                // 3d array for characters
+                char[,,] letters = 
+                {
+                    {
+                        {'A' , spade},
+                        {'B' , spade},
+                        {'C' , spade},
+                        {'D' , spade},
+                        {'E' , spade},
+                        {'F' , spade}
+                    },
+                    {
+                        {'G' , spade},
+                        {'H' , spade},
+                        {'I' , spade},
+                        {'J' , spade},
+                        {'K' , spade},
+                        {'L' , spade}
+                    },
+                    {
+                        {'M' , spade},
+                        {'N' , spade},
+                        {'O' , spade},
+                        {'P' , spade},
+                        {'Q' , spade},
+                        {'R' , spade}
+                    },
+                    {
+                        {'S' , spade},
+                        {'T' , spade},
+                        {'U' , spade},
+                        {'V' , spade},
+                        {'W' , spade},
+                        {'X' , spade}
+                    },
+
+                };
+
+            
+                // creates table/grid of letters
+                for(int rows = 0; rows != 4; rows++)
+                {
+                    Console.WriteLine("");
+                    for(int columns = 0; columns != 6; columns++)
+                    {
+                        Console.Write(" | " + letters[rows,columns,0] + " | ");                
+                    }    
+                }
+
+                //user stats
+                Console.WriteLine("hearts found: " + heartsFound + " out  of " + totalHearts);
+                Console.WriteLine("SCORE: " + score);
+
+                //places a random heart under a letter
+                for(int userOpt = 0; userOpt != choice; userOpt++)
+                {
+                Random rnd = new Random();
+                var randomNum = rnd.Next(0, letters.Length);
+                randomNum = letters[letters.Length,letters.Length,heart];
+                }
+                
+               
+                // user input
+                int count = 0; //counts amount of times user inputs letter
+                do
+                {
+                Console.WriteLine("please choose a letter from the grid");
+                char userInput;
+                userInput = Convert.ToChar(Console.ReadLine());
+                count++;
+                //binary search algo
+
+                int minVal = 0;
+                int maxVal = letters.Length - 1;
+                var key = userInput;
+                while(minVal <= maxVal)
+                {
+                    int mid = (minVal + maxVal) / 2;
+                    if (key == mid)
+                    {
+                        letters[0,0, letters.Length] = spade;
+
+                    }
+                    else if (key < mid)
+                    {
+                        maxVal = mid - 1;
+                    }
+                    else
+                    {
+                        minVal = mid - 1;
+                    }    
+                }
+                
+
+                }while(heartsFound != totalHearts);
+
+                if(heartsFound == totalHearts)
+                {
+                    using(StreamWriter writer = new StreamWriter("gameStatistics.txt"))
+                    {
+                        score = heartsFound / count;
+                        Console.WriteLine("your score is: " + score);
+                        Console.WriteLine("total hearts found: " + totalHearts);
+                    }
+                }
+            }
+            while(letsPlay == "y");
+
+            if( letsPlay == "n")
+            {
+                Console.WriteLine("okay bye");
+            }
+            
+            
+            
+
+          // unfinished 
+
+             
+
+
+
+        }
+    }
+}
